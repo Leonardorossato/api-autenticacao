5db3835e0b382e5d7df4b8260233395f314480e7
Leonardorossato <leonardorossato21@gmail.com>
2023-09-04 17:17:56 -0300

    swagger api added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Api Authentication Backend Nestjs Api')
+    .setDescription('Api Authentication with NestJs, Swagger and Jwt')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.APP_PORT);
 }
-bootstrap();
+void bootstrap();
