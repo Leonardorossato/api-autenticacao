d751651081b87735c147e11011643eee48d5a263
Leonardorossato <leonardorossato21@gmail.com>
2023-09-04 17:18:25 -0300

    feat: create envoriment.ts

---
+++ src/config/envoriment.ts
@@ -0,0 +1,11 @@
+export {};
+
+// Here we declare the members of the process.env object, so that we
+// can use them in our application code in a type-safe manner.
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      APP_PORT: number;
+    }
+  }
+}
