3daecd01347f22f1d6113cc339a65d08ac9c9520
Leonardorossato <leonardorossato21@gmail.com>
2023-09-04 17:17:46 -0300

    imports added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
 })
 export class AppModule {}
