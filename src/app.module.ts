3de10294db435bf6f96a88bc186f567510c83e33
Leonardorossato <leonardorossato21@gmail.com>
2023-09-05 14:05:48 -0300

    fixed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     JwtModule.register({
-      secret: `${process.env.JWT_SECRET}`,
+      global: true,
+      secret: process.env.JWT_SECRET,
       signOptions: {
         expiresIn: '1h',
       },
