4476ee8beced8f8aa3f1751dba350e762ab487f8
Leonardorossato <leonardorossato21@gmail.com>
2023-09-05 13:25:45 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Api Authentication Backend Nestjs Api')
     .setDescription('Api Authentication with NestJs, Swagger and Jwt')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
